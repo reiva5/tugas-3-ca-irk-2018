b4804a88e5427b158d3ae9ad0b2b55a463787894
ihsansaktia <ihsan.saktia@gmail.com>
2018-08-08 22:04:36 +0700

    Add setter for boardsize and bombs in gamecontroller

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -1,11 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
 
     private static GameController instance = null;
+	
+    [SerializeField]
+    private int boardSize = -1;
+
+    [SerializeField]
+    private int bombs = -1;
 
     public static GameController Instance
     {
@@ -17,6 +22,32 @@ public class GameController : MonoBehaviour
         set
         {
             instance = value;
+        }
+    }
+
+    public int BoardSize
+    {
+        get
+        {
+            return boardSize;
+        }
+
+        set
+        {
+            boardSize = value;
+        }
+    }
+
+    public int Bombs
+    {
+        get
+        {
+            return bombs;
+        }
+
+        set
+        {
+            bombs = value;
         }
     }
 
@@ -41,5 +72,17 @@ public class GameController : MonoBehaviour
     void Update()
     {
 
+    }
+
+    public void SetBoardSize(Text boardsize)
+    {
+        BoardSize = int.Parse(boardsize.text);
+		BoardSize = Mathf.Clamp(BoardSize, 1, 100);
+    }
+
+    public void SetBombs(Text bombs)
+    {
+        Bombs = int.Parse(bombs.text);
+		Bombs = Mathf.Clamp(Bombs, 1, 100);
     }
 }
