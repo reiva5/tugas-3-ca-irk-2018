5c5568659b85cfb1b8c68419b4693806ffffc6ea
ihsansaktia <ihsan.saktia@gmail.com>
2018-08-08 21:23:54 +0700

    Add gameview in mainmenu scene

---
+++ Assets/GameView.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameView : MonoBehaviour {
+
+	private static GameView instance = null;
+
+	void Awake() {
+		if (instance == null) {
+			instance = this;
+			DontDestroyOnLoad(this.gameObject);
+			return;
+		}
+		Destroy(this.gameObject);
+	}
+
+	[SerializeField]
+	private Sprite[] cellSprite;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
