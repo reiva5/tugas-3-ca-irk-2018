561eb6910934d80872e878357aa79a39afc88160
ihsansaktia <ihsan.saktia@gmail.com>
2018-08-08 21:07:43 +0700

    Add gamecontroller

---
+++ Assets/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	private static GameController instance = null;
+
+	void Awake() {
+		if (instance == null) {
+			instance = this;
+			DontDestroyOnLoad(this.gameObject);
+			return;
+		}
+		Destroy(this.gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
