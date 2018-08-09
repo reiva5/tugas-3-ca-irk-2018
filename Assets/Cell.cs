10c1ec329fc2f1c62f333fd63f171cfe84fd1af5
ihsansaktia <ihsan.saktia@gmail.com>
2018-08-09 14:10:38 +0700

    Add method when a cell is clicked

--- Assets/Cell.cs
+++ Assets/Cell.cs
@@ -3,18 +3,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cell : MonoBehaviour {
+public class Cell : MonoBehaviour
+{
 
-	[SerializeField]
-	private bool isThisMine;
+    [SerializeField]
+    private bool isThisMine;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void CellClick()
+    {
+        BoardController.Instance.CellClick(this.transform.GetSiblingIndex());
+    }
 }
