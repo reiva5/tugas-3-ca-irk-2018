943ba4e407d2fb48a0bfabf0e512ec80d0f48af0
ihsansaktia <ihsan.saktia@gmail.com>
2018-08-09 17:39:16 +0700

    Tidying code in cell.cs

--- Assets/Cell.cs
+++ Assets/Cell.cs
@@ -1,32 +1,14 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class Cell : MonoBehaviour
 {
-
-    [SerializeField]
-    private bool isThisMine;
-
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     public void CellClick()
     {
         if (BoardController.Instance.IsWaitingForInput())
         {
-            BoardController.Instance.CellClick(this.transform.GetSiblingIndex());
+            int cellindex = this.transform.GetSiblingIndex();
+            BoardController.Instance.CellClick(cellindex);
         }
     }
 
