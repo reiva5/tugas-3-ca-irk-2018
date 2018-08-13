c3b472e2da1cc0b499b0c6b470a56b10b2038616
Tony <buddy90_lost@yahoo.co.id>
2018-08-13 12:53:54 +0700

    optimize code

--- cli/main.cpp
+++ cli/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <ctime>
 #include <vector>
 #include <cstdlib>
 #include <queue>
@@ -14,19 +15,19 @@ void create_table(vector<vector<T>> &tmp, int n, int m, T val){
 	tmp.assign(n, vector<T>(m, val));
 }
 void printTitle();
-void fill_bomb(int, int, int, vector<vector<bool>> &);
+void fill_bomb(int, int, int, vector<vector<char>> &);
 void readTableSize(int &, int &);
 void readNumberOfBomb(int, int, int &);
-void fill_numbTable(int, int, vector<vector<short>> &, const vector<vector<bool>> &);
-void printMap(int, int, const vector<vector<short>> &, const vector<vector<bool>> &, bool);
+void fillMapTable(int, int, vector<vector<char>> &);
+void printMap(int, int, const vector<vector<char>> &, const vector<vector<bool>> &, bool);
 void readCoordinateInput(int, int, int &, int &);
 void readPlayAgainResponse(bool &);
-void open_cell(int, int, int, int, const vector<vector<short>> &, vector<vector<bool>> &, int &);
+void open_cell(int, int, int, int, const vector<vector<char>> &, vector<vector<bool>> &, int &);
 void refreshScreen();
 
 int main(){
-	vector<vector<bool>> isopen, isbomb;
-	vector<vector<short>> numb;
+	vector<vector<bool>> isopen;
+	vector<vector<char>> map;
 	int n, m, nbomb, rem_block;
 	bool playing;
 	int x, y;
@@ -36,29 +37,28 @@ int main(){
 		playing = false;
 		readTableSize(n ,m);
 		create_table(isopen, n, m, false);
-		create_table(isbomb, n, m, false);
-		create_table(numb, n, m, (short)0);
+		create_table(map, n, m, '\0');
 		readNumberOfBomb(n, m, nbomb);
 		rem_block = n*m-nbomb;
-		fill_bomb(n, m, nbomb, isbomb);
-		fill_numbTable(n, m, numb, isbomb);
+		fill_bomb(n, m, nbomb, map);
+		fillMapTable(n, m, map);
 		while(rem_block > 0){
 			refreshScreen();
-			printMap(n, n, numb, isopen, false);
+			printMap(n, n, map, isopen, false);
 			readCoordinateInput(n, m, x, y);
 			while(isopen[x][y]){
 				cout<<"Kotak telah dibuka, ";
 				readCoordinateInput(n, m, x, y);
 			}
-			if(isbomb[x][y])
+			if(map[x][y] == -1)
 				break;
-			open_cell(x, y, n, m, numb, isopen, rem_block);
+			open_cell(x, y, n, m, map, isopen, rem_block);
 		}
 		if(rem_block == 0)
 			cout<<"Selamat, Anda Menang!\n";
 		else{
 			cout<<"Anda Kalah!\n";
-			printMap(n, n, numb, isopen, true);
+			printMap(n, n, map, isopen, true);
 		}
 		readPlayAgainResponse(playing);
 	}while(playing);
@@ -71,14 +71,14 @@ void printTitle(){
 	cout<<"////////////////////////////////\n\n";
 }
 
-void fill_bomb(int n, int m, int num, vector<vector<bool>> &isbomb){
+void fill_bomb(int n, int m, int num, vector<vector<char>> &map){
 	while(num){
 		int x, y;
 		do{
 			x = rand()%n;
 			y = rand()%m;
-		}while(isbomb[x][y]);
-		isbomb[x][y] = true;
+		}while(map[x][y] == -1);
+		map[x][y] = -1;
 		num--;
 	}
 }
@@ -95,41 +95,38 @@ void readTableSize(int &n, int &m){
 }
 
 void readNumberOfBomb(int n, int m, int &nbomb){
-	cout<<"Masukkan banyak bomb : "; cin>>nbomb;
-	while(nbomb<0 || nbomb>n*m){
-		cout<<"Masukkan salah! masukkan lagi banyak bomb : "; cin>>nbomb;
+	cout<<"Masukkan banyak bomb [0-"<<n*m-1<<"] : "; cin>>nbomb;
+	while(nbomb<0 || nbomb>=n*m){
+		cout<<"Masukkan salah! masukkan lagi banyak bomb [0-"<<n*m-1<<"] : "; cin>>nbomb;
 	}
 }
 
-void fill_numbTable(int n, int m, vector<vector<short>> &numb, const vector<vector<bool>> &isbomb){
+void fillMapTable(int n, int m, vector<vector<char>> &map){
 	for(int i=0;i<n;++i){
 		for(int j=0;j<m;++j){
-			if(isbomb[i][j])
-				numb[i][j] = -1;
-			else{
-				numb[i][j] = 0;
+			if(map[i][j] != -1){
 				for(int k=0;k<8;++k){
 					int xx = i+dx[k];
 					int yy = j+dy[k];
-					if(xx>=0 && xx<n && yy>=0 && yy<m && isbomb[xx][yy])
-						++numb[i][j];
+					if(xx>=0 && xx<n && yy>=0 && yy<m && map[xx][yy] == -1)
+						++map[i][j];
 				}
 			}
 		}
 	}
 }
 
-void printMap(int n, int m, const vector<vector<short>> & numb, const vector<vector<bool>> &isopen, bool openbomb){
+void printMap(int n, int m, const vector<vector<char>> & map, const vector<vector<bool>> &isopen, bool openbomb){
 	cout<<"\\X\n";
 	cout<<"Y";
 	for(int i=0;i<n;++i){
 		if(i > 0)
 			cout<<" ";
 		for(int j=0;j<m;++j){
-			if(numb[i][j] == -1 && openbomb)
+			if(openbomb && map[i][j] == -1)
 				cout<<"*";
 			else if(isopen[i][j])
-				cout<<numb[i][j];
+				cout<<static_cast<int>(map[i][j]);
 			else
 				cout<<"#";
 		}
@@ -159,7 +156,7 @@ void readPlayAgainResponse(bool &playing){
 		playing = true;
 }
 
-void open_cell(int x, int y, int n, int m, const vector<vector<short>> &numb, vector<vector<bool>> &isopen, int &rem_block){
+void open_cell(int x, int y, int n, int m, const vector<vector<char>> &map, vector<vector<bool>> &isopen, int &rem_block){
 	queue<pair<int, int>> q;
 	q.push({x, y});
 	isopen[x][y] = true;
@@ -167,7 +164,7 @@ void open_cell(int x, int y, int n, int m, const vector<vector<short>> &numb, ve
 		pair<int, int> tmp = q.front();
 		q.pop();
 		--rem_block;
-		if(numb[tmp.first][tmp.second]==0){
+		if(map[tmp.first][tmp.second]==0){
 			for(int i=0;i<8;++i){
 				int xx = tmp.first+dx[i];
 				int yy = tmp.second+dy[i];
